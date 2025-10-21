a6a3ab94ec396f005751fee6624a6cb19c99dd2e
Arslan Ahmed <111530185+arslan020@users.noreply.github.com>
2025-10-21 23:43:15 +0100

    Update Staff schema to make firstName and lastName optional

--- models/Staff.js
+++ models/Staff.js
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const StaffSchema = new mongoose.Schema({
-  firstName: { type: String, required: true },
-  lastName:  { type: String, required: true },
-  username:  { type: String, unique: true, required: true },
-  email:     { type: String, unique: true, required: true },
-  passwordHash: { type: String, required: true }
+  firstName: { type: String, required: false, default: undefined },
+  lastName:  { type: String, required: false, default: undefined },
+  username:  { type: String, required: true, unique: true, trim: true },
+  email:     { type: String, required: true, unique: true, trim: true },
+  passwordHash: { type: String, required: true },
 }, { timestamps: true });
 
 export default mongoose.model('Staff', StaffSchema);
